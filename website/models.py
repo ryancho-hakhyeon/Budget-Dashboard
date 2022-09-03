cbaf5d92fee7b7352635d45bd3ad443e37546afd
ryancho-hakhyeon <ryancho.hakhyeon@gmail.com>
2022-09-02 23:38:51 -0700

    shaping datasets

--- website/models.py
+++ website/models.py
@@ -2,26 +2,34 @@ import pandas as pd
 
 
 class Dataset:
+    import pandas as pd
     def __int__(self, year, month):
         self.year = year
         self.month = month
 
     def get_total_data(self):
-        data = pd.read_csv('../dataset.csv')
+        data = pd.read_csv('./dataset.csv')
         # df = data['year'].unique()
 
-        income_each_year = data.groupby('year')['income'].sum()
-        outcome_each_year = data.groupby('year')['outcome'].sum()
+        income_each_year = data.groupby('year')['income'].sum().to_dict()
+        outcome_each_year = data.groupby('year')['outcome'].sum().to_dict()
 
-        income_year_month = data.groupby(['year', 'month'])['income'].sum()
-        outcome_year_month = data.groupby(['year', 'month'])['outcome'].sum()
+        total_year_month = data.groupby(['year', 'month']).agg(
+                total_income=('income', 'sum'),
+                total_outcome=('outcome', 'sum')
+            ).reset_index().to_dict()
 
-        return outcome_year_month
+        # The same result
+        # total_year_month = data.groupby(['year', 'month']).agg({'income':'sum', 'outcome':'sum' })
+        # total_year_month = data.groupby(['year', 'month'])[['income', 'outcome']].sum()
+
+        return income_each_year, outcome_each_year, total_year_month
 
     def get_filter_data(self):
-        data = pd.read_csv('../dataset.csv')
+        data = pd.read_csv('./dataset.csv')
 
-        return data
+        filtered_data = data.loc[(data['year'] == self.year) & (data['month'] == self.month)]
+        return filtered_data
 
     def set_year(self, year):
         self.year = year
@@ -30,7 +38,14 @@ class Dataset:
         self.month = month
 
 
-temp = Dataset()
+# temp = Dataset()
+#
+# # get total
+# income, outcome, total = temp.get_total_data()
+#
+# print(list(outcome.values()))
 
-
-print(temp.get_total_data())
+# temp.set_year(2022)
+# temp.set_month('Jul')
+# temp.get_filter_data()['date'].values.tolist()
+# print(temp.get_filter_data())
--- website/views.py
+++ website/views.py
@@ -6,35 +6,62 @@ views = Blueprint('views', __name__)
 
 
 # home page
-@views.route('/')
-@views.route('home')
+@views.route('/', methods=['GET'])
+@views.route('Home', methods=['GET'])
 def home_page():
-    return render_template('home.html', )
+    chart_datasets = dict()
+    datasets = Dataset()
+
+    income, outcome, total = datasets.get_total_data()
+    total_income = sum(income.values())
+    total_outcome = round(sum(outcome.values()), 2)
+    current_balance = total_income - total_outcome
+    chart_datasets = {
+        'income_each_year': {
+            'title': '',
+            'data': income.values(),
+            'label': income.keys()
+        },
+        'total_income': '{:,.2f}'.format(total_income),
+        'outcome_each_year': {
+            'title': '',
+            'data': outcome.values(),
+            'label': outcome.keys()
+        },
+        'total_outcome': '{:,.2f}'.format(total_outcome),
+        'current_balance': '{:,.2f}'.format(current_balance),
+        'total_each_month': total
+    }
+    return render_template('home.html', dataset=chart_datasets)
 
 
 @views.route('/Analytics', methods=['POST', 'GET'])
 def analytics_page():
-    datasets = Dataset().get_data()
-    new_date = datasets.loc[(datasets['year'] == 2022) & (datasets['month'] == 'Aug')]
-    print(new_date)
+    datasets = Dataset()
 
     if request.method == 'POST':
         year = request.form.get('line_year')
         month = request.form.get('line_month')
+        # setting year and month into Dataset
+        datasets.set_year(year)
+        datasets.set_month(month)
+
+        new_data = datasets.get_filter_data()
+
         date = f'{year} {month}'
-        dataset = {
+        chart_datasets = {
             'title': date,
             'data': [12, 19, 3, 5, 2, -3],
             'labels': ['Red', 'Blue', 'Yellow', 'Green', 'Purple', 'Orange']
         }
-        return render_template('analytics.html', dataset=dataset)
+        return render_template('analytics.html', dataset=chart_datasets)
     else:
-        dataset = {
+        chart_datasets = {
             'title': 'AUGUST DETAIL',
             'data': [12, 19, 3, 5, 2, -3],
             'labels': ['Red', 'Blue', 'Yellow', 'Green', 'Purple', 'Orange']
         }
-        return render_template('analytics.html', dataset=dataset)
+        return render_template('analytics.html', dataset=chart_datasets)
 
 
 @views.route('/Analytics-pie')
